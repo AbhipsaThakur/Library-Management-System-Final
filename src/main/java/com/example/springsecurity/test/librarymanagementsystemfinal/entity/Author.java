97ff2e27acdfe965d4d13ac4c942efe7f491537f
AbhipsaThakur <abhipsathakur933@gmail.com>
2024-11-15 18:38:49 +0530

    Update Author.java

--- src/main/java/com/example/springsecurity/test/librarymanagementsystemfinal/entity/Author.java
+++ src/main/java/com/example/springsecurity/test/librarymanagementsystemfinal/entity/Author.java
@@ -1,4 +1,51 @@
 package com.example.springsecurity.test.librarymanagementsystemfinal.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "author")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "authorid", length = 11)
+    private int authorid;
+
+    @Column(name = "authorname", length = 45)
+    private String authorname;
+
+    public Author(int authorid, String authorname) {
+        this.authorid = authorid;
+        this.authorname = authorname;
+    }
+
+    public Author(String authorname) {
+        this.authorname = authorname;
+    }
+    public Author() {
+
+    }
+
+    public int getAuthorid() {
+        return authorid;
+    }
+
+    public void setAuthorid(int authorid) {
+        this.authorid = authorid;
+    }
+
+    public String getAuthorname() {
+        return authorname;
+    }
+
+    public void setAuthorname(String authorname) {
+        this.authorname = authorname;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "authorid=" + authorid +
+                ", authorname='" + authorname + '\'' +
+                '}';
+    }
 }
